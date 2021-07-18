e581983ec2da41316288ba4c316396895f2a7727
Arthur P <arts.mane@gmail.com>
2021-07-18 11:12:10 -0300

    testing api

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -9,3 +10,17 @@ def index():
 @app.route('/oi')
 def oi_mundo():
     return "<p>Oi, mundo!!!</p>"
+
+@app.route('/api', methods=['GET', 'POST'])
+def api():
+    if request.method=='POST':
+        return 'Invalid Method. Please use GET'
+    else:
+        name = request.args.get('name')
+        age = request.args.get('age')
+        response = {
+            'name':name,
+            'age':age
+            }
+        return response
+
